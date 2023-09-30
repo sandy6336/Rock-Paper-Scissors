9f725d2740f186018495970cabd080347dac1056
sandy6336 <112769831+sandy6336@users.noreply.github.com>
2023-09-30 09:17:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -54,10 +54,11 @@ function playGame(playerMove) {
         .innerHTML = `${result}`;
 
     document.querySelector('.js-moves')
-        .innerHTML = `You
-        <img class="css-img" src="./Images/${playerMove}-emoji.png" alt="">
-        <img class="css-img" src="./Images/${computerMove}-emoji.png" alt="">
-        Computer`;
+        .innerHTML = 'You' +
+    '<img class="css-img" src="./Images/' + playerMove + '-emoji.png" alt="">' +
+    '<img class="css-img" src="./Images/' + computerMove + '-emoji.png" alt="">' +
+    'Computer';
+
 
     if(result === 'You Won')  {
         score.wins += 1;
